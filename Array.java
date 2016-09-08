e3756511855a262a1a19a0395ab705660ed4a93e
thilakas <thilikas29@gmail.com>
2016-09-08 12:42:56 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,71 @@
+package logic;
+
+import java.util.Scanner;
+
+public class Array {
+	static java.lang.String romanNumeral;
+    static int decimalNum;
+    public static void main(String args[]) {
+            Array rmtoD = new Array();
+            rmtoD .convertToDecimal();
+            rmtoD .printRoman(romanNumeral);
+    }
+
+    public void convertToDecimal () {
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Enter a Roman number: ");
+            romanNumeral = scan.nextLine();
+            romanNumeral = romanNumeral.toUpperCase();
+            
+            int l=  romanNumeral.length();
+            int num=0;
+            int previousnum = 0;
+            for (int i=l-1;i>=0;i--)
+            { 
+                    char x =  romanNumeral.charAt(i);
+                    x = Character.toUpperCase(x);
+                    switch(x)
+                    {  
+                            case 'I':
+                            previousnum = num;
+                            num = 1;
+                            break;
+                         case 'V':
+                                 previousnum = num;
+                            num = 5;
+                            break;
+                            case 'X':
+                                    previousnum = num;
+                            num = 10;
+                            break;
+                            case 'L':
+                                    previousnum = num;
+                            num = 50;
+                            break;
+                            case 'C':
+                                    previousnum = num;
+                            num = 100;
+                            break;
+                            case 'D':
+                                    previousnum = num;
+                            num = 500;
+                            break;
+                            case 'M':
+                                    previousnum = num;
+                            num = 1000;
+                            break;
+                    }           
+                    if (num<previousnum)
+                    {decimalNum= decimalNum-num;}
+                     else
+                    decimalNum= decimalNum+num;
+            }
+    }
+    public static void printRoman (java.lang.String romanNumeral2){
+            System.out.println ("The equivalent of the Roman numeral "+romanNumeral2+" is "+decimalNum);
+    }
+
+    public static void printDecimal (int decimalNum){
+            System.out.println ("Decimal Number stored is: " + decimalNum);
+    }
+}
